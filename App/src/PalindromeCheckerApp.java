3242b9ee2c492452d856068736606fc2c1039680
pg2398-glitch <pg2398@srmist.edu.in>
2026-03-05 18:39:05 +0530

    [GANA SRI] Added USECASE RELATED NAME

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,6 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.printf("Welcome to Palindrone Checker App Management Ststem");
+        System.out.println("Modify this logic to usecase 2");
+    }
+}
